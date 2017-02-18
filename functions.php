7863eaeb821a125f221e83fb9f4cbe95a22b1d71
JoWain <linus.ritzmann@gmail.com>
2017-02-18 17:31:41 +0100

    New CSS file
    Wrote about half of the new flexbox-based CSS

--- functions.php
+++ functions.php
@@ -18,6 +18,19 @@ add_action( 'init' , 'tvf_ns_theme_setup' );
 
 /*
 ======================
+FONT ENQUEUE
+======================
+*/
+
+function wpb_add_google_fonts() {
+
+wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css?family=Abel|Yanone+Kaffeesatz:300,400|PT+Serif', false );
+}
+
+add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
+
+/*
+======================
 SCRIPT ENQUEUE
 ======================
 */
@@ -26,10 +39,11 @@ function tvf_script_enqueue() {
 
 	//	stylesheets enqueue
 	//wp_enqueue_style( 'toastgrid' , get_template_directory_uri() . '/css/toast/grid.css', array() , '1.0.0' , 'all' );
-	wp_enqueue_style( 'customstyle' , get_template_directory_uri() . '/css/turnverein.css', array() , '1.0.0' , 'all' );
+
+	wp_enqueue_style( 'customstyle' , get_template_directory_uri() . '/css/turnverein-2.css', array() , '1.0.0' , 'all' );
 
 	// javascript enqueue
-	//wp_enqueue_script('custom-jquery' , get_template_directory_uri() . '/js/jquery.min.js' , '2.1.4' , true );
+	wp_enqueue_script('custom-jquery' , get_template_directory_uri() . '/js/jquery.min.js' , '2.1.4' , true );
 
   wp_enqueue_script('customjs' , get_template_directory_uri() . '/js/turnverein.js' , '1.0.0' , true );
 
@@ -60,7 +74,7 @@ SIDEBAR FUNCTION
 			'name'					=>	'Rechte Seitenleiste TV',
 			'id'						=>	'widgets-right-tv',
 			'class'					=>	'custom-wid',
-			'description'		=>	'Dieses Widget kann nach belieben befüllt werden',
+			'description'		=>	'Dieses Widget erscheint nur auf der TV-seite',
 			'before_widget'	=>	'<aside id="%1$s" class="widget widget-rechts %2$s">',
 			'after_widget'	=>	'</aside>',
 			'before_title'	=>	'<h1 class="widget-title-rechts">',
@@ -176,44 +190,44 @@ SIDEBAR FUNCTION
 		)
 	);
 
-	register_sidebar(
-			array(
-			'name'					=>	'Seitenende Feld 3',
-			'id'						=>	'bottomline-3',
-			'class'					=>	'custom-wid',
-			'description'		=>	'Dieses Widget erscheint ganz unten an der Seite und wird für Kontakte usw. gebraucht.',
-			'before_widget'	=>	'<aside id="%1$s" class="widget widget-unten %2$s">',
-			'after_widget'	=>	'</aside>',
-			'before_title'	=>	'<h1 class="bottomline-title">',
-			'after_title'		=>	'</h1>'
-		)
-	);
-
-	register_sidebar(
-			array(
-			'name'					=>	'Seitenende Feld 4',
-			'id'						=>	'bottomline-4',
-			'class'					=>	'custom-wid',
-			'description'		=>	'Dieses Widget erscheint ganz unten an der Seite und wird für Kontakte usw. gebraucht.',
-			'before_widget'	=>	'<aside id="%1$s" class="widget widget-unten %2$s">',
-			'after_widget'	=>	'</aside>',
-			'before_title'	=>	'<h1 class="bottomline-title">',
-			'after_title'		=>	'</h1>'
-		)
-	);
-
-	register_sidebar(
-			array(
-			'name'					=>	'Seitenende Feld 5',
-			'id'						=>	'bottomline-5',
-			'class'					=>	'custom-wid',
-			'description'		=>	'Dieses Widget erscheint ganz unten an der Seite und wird für Kontakte usw. gebraucht.',
-			'before_widget'	=>	'<aside id="%1$s" class="widget widget-unten %2$s">',
-			'after_widget'	=>	'</aside>',
-			'before_title'	=>	'<h1 class="bottomline-title">',
-			'after_title'		=>	'</h1>'
-		)
-	);
+	// register_sidebar(
+	// 		array(
+	// 		'name'					=>	'Seitenende Feld 3',
+	// 		'id'						=>	'bottomline-3',
+	// 		'class'					=>	'custom-wid',
+	// 		'description'		=>	'Dieses Widget erscheint ganz unten an der Seite und wird für Kontakte usw. gebraucht.',
+	// 		'before_widget'	=>	'<aside id="%1$s" class="widget widget-unten %2$s">',
+	// 		'after_widget'	=>	'</aside>',
+	// 		'before_title'	=>	'<h1 class="bottomline-title">',
+	// 		'after_title'		=>	'</h1>'
+	// 	)
+	// );
+	//
+	// register_sidebar(
+	// 		array(
+	// 		'name'					=>	'Seitenende Feld 4',
+	// 		'id'						=>	'bottomline-4',
+	// 		'class'					=>	'custom-wid',
+	// 		'description'		=>	'Dieses Widget erscheint ganz unten an der Seite und wird für Kontakte usw. gebraucht.',
+	// 		'before_widget'	=>	'<aside id="%1$s" class="widget widget-unten %2$s">',
+	// 		'after_widget'	=>	'</aside>',
+	// 		'before_title'	=>	'<h1 class="bottomline-title">',
+	// 		'after_title'		=>	'</h1>'
+	// 	)
+	// );
+	//
+	// register_sidebar(
+	// 		array(
+	// 		'name'					=>	'Seitenende Feld 5',
+	// 		'id'						=>	'bottomline-5',
+	// 		'class'					=>	'custom-wid',
+	// 		'description'		=>	'Dieses Widget erscheint ganz unten an der Seite und wird für Kontakte usw. gebraucht.',
+	// 		'before_widget'	=>	'<aside id="%1$s" class="widget widget-unten %2$s">',
+	// 		'after_widget'	=>	'</aside>',
+	// 		'before_title'	=>	'<h1 class="bottomline-title">',
+	// 		'after_title'		=>	'</h1>'
+	// 	)
+	// );
 
 
  }
--- index.php
+++ index.php
@@ -37,9 +37,18 @@
 
 				?>
 
-				<div id="titelbild" class="header-bild-container">
+				<div id="titelbild" style="background-image: url(<?php the_post_thumbnail_url( 'full' ); ?>);
+				">
 
-					<?php the_post_thumbnail( 'large' ); ?>
+					<span class="logo">Logo!</span>
+
+				</div>
+
+				<div id="titel-schriftzug">
+
+					<h1>
+						<?php the_title(); ?>
+					</h1>
 
 				</div>
 
@@ -54,22 +63,98 @@
 	</div>
 	<!-- end page top -->
 
-	<div id="page-main" class="grid-container">
-		<!-- grid container -->
+	<div id="page-main" class="flex-container">
 
-		<div class="grid">
-			<!-- grid -->
+		<div class="table-of-contents box">
 
-			<div class="table-of-contents grid__col grid__col--1-of-5">
+			<!-- Here we build the ToC -->
 
-				<!-- Here we will build the ToC -->
-				<div class="box">
+			<?php
 
+			$tvf_subpages_query = new WP_Query(
 
+				array(
+					'post_type' => 'page' ,
+					'posts_per_page' => -1 ,
+					'post_parent' => $tvf_page_id ,
+					'order' => 'ASC' ,
+					'orderby' => 'menu_order'
+				)
+
+			);
+
+			$tvf_blog_query = new WP_Query( array( 'category_name' => $tvf_page_slug ));
+
+			if ( $tvf_blog_query->have_posts() ) {
+
+				echo '<a class="localnav haveablog" href="#blogtitle">Blog</a>';
+
+			}
+
+			if ( $tvf_subpages_query->have_posts() ) {
+
+				while ( $tvf_subpages_query->have_posts() ) { $tvf_subpages_query->the_post();
+					?>
+
+					<a class="localnav" href="#<?php the_ID(); ?>title"><?php the_title(); ?></a>
 
 					<?php
+				}
 
-					$tvf_subpages_query = new WP_Query(
+			}
+
+			wp_reset_postdata();
+
+			?>
+
+
+		</div>
+		<!-- end table of contents -->
+
+		<div class="page-content box">
+
+			<!-- Here we print the page content -->
+
+			<!-- if we have a blog we print it here -->
+
+			<?php
+
+			$tvf_blog_query = new WP_Query( array( 'category_name' => $tvf_page_slug ));
+
+			if ( $tvf_blog_query->have_posts() ) {
+
+				echo '<div id="blog-wrapper" class="content-part">';
+
+				echo '<h1 id="blogtitle" class="haveablog">Blog</h1>';
+
+			}
+
+			if ( $tvf_blog_query->have_posts() ) {
+
+				while ( $tvf_blog_query->have_posts() ) { $tvf_blog_query->the_post();
+					?>
+
+					<h2><?php the_title(); ?></h2>
+					<p><?php the_content(); ?></p>
+
+					<?php
+				}
+
+			}
+
+			if ( $tvf_blog_query->have_posts() ) {
+
+				echo '</div>';
+
+			}
+
+			wp_reset_postdata();
+			?>
+
+			<!-- now we print the subpages -->
+			<?php
+
+				$tvf_subpages_query = new WP_Query(
 
 					array(
 						'post_type' => 'page' ,
@@ -81,89 +166,13 @@
 
 				);
 
-				$tvf_blog_query = new WP_Query( array( 'category_name' => $tvf_page_slug ));
-
-				if ( $tvf_blog_query->have_posts() ) {
-
-					echo '<a class="localnav haveablog" href="#blogtitle">Blog</a>';
-
-				}
-
 				if ( $tvf_subpages_query->have_posts() ) {
 
 					while ( $tvf_subpages_query->have_posts() ) { $tvf_subpages_query->the_post();
 						?>
 
-						<a class="localnav" href="#<?php the_ID(); ?>title"><?php the_title(); ?></a>
+						<div class="content-part">
 
-						<?php
-					}
-
-				}
-
-				wp_reset_postdata();
-
-				?>
-			</div>
-			</div>
-			<!-- end table of contents -->
-
-			<div class="page-content grid__col grid__col--3-of-5">
-
-			<div class="box">
-
-				<div class="blog-wrapper">
-
-
-					<?php
-
-					$tvf_blog_query = new WP_Query( array( 'category_name' => $tvf_page_slug ));
-
-					if ( $tvf_blog_query->have_posts() ) {
-
-						echo '<h1 id="blogtitle" class="haveablog">Blog</h1>';
-
-					}
-
-					if ( $tvf_blog_query->have_posts() ) {
-
-						while ( $tvf_blog_query->have_posts() ) { $tvf_blog_query->the_post();
-							?>
-
-							<h2><?php the_title(); ?></h2>
-							<p><?php the_content(); ?></p>
-
-							<?php
-						}
-
-					}
-
-					wp_reset_postdata();
-					?>
-
-				</div>
-
-				<div class="subpages-wrapper">
-
-
-				<?php
-
-					$tvf_subpages_query = new WP_Query(
-
-						array(
-							'post_type' => 'page' ,
-							'posts_per_page' => -1 ,
-							'post_parent' => $tvf_page_id ,
-							'order' => 'ASC' ,
-							'orderby' => 'menu_order'
-						)
-
-					);
-
-					if ( $tvf_subpages_query->have_posts() ) {
-
-						while ( $tvf_subpages_query->have_posts() ) { $tvf_subpages_query->the_post();
-							?>
 							<figure class="subpageimage">
 
 								<?php the_post_thumbnail(); ?>
@@ -177,78 +186,72 @@
 
 							</article>
 
-							<?php
-						}
+						</div>
 
+						<?php
 					}
 
-					wp_reset_postdata();
+				}
+
+				wp_reset_postdata();
+
+			?>
+
+		</div>
+		<!-- end page content -->
+
+		<div class="widget-area box">
+
+			<!-- Here we establish the Widgets -->
+
+				<?php
+
+				switch ($tvf_page_slug) {
+					case 'turnverein':
+					dynamic_sidebar('widgets-right-tv');
+					break;
+
+					case 'faustball':
+					dynamic_sidebar('widgets-right-fb');
+					break;
+
+					case 'jugendriege':
+					dynamic_sidebar('widgets-right-jr');
+					break;
+
+					case 'madchenriege':
+					dynamic_sidebar('widgets-right-maer');
+					break;
+
+					case 'mannerriege':
+					dynamic_sidebar('widgets-right-mr');
+					break;
+
+					case 'damenturnverein':
+					dynamic_sidebar('widgets-right-dtv');
+					break;
+
+					case 'elki-turnen':
+					dynamic_sidebar('widgets-right-elki');
+					break;
+
+					default:
+					echo '<p>Keine Seitenleiste eingerichtet</p>';
+					break;
+				}
 
 				?>
 
-				</div>
-
-			</div>
-
-			</div>
-			<!-- end page content -->
-
-			<div class="widget-area grid__col grid__col--1-of-5">
-
-				<!-- Here we will establish the Widgets -->
-				<div class="box">
-					<?php
-
-					switch ($tvf_page_slug) {
-						case 'turnverein':
-						dynamic_sidebar('widgets-right-tv');
-						break;
-
-						case 'faustball':
-						dynamic_sidebar('widgets-right-fb');
-						break;
-
-						case 'jugendriege':
-						dynamic_sidebar('widgets-right-jr');
-						break;
-
-						case 'madchenriege':
-						dynamic_sidebar('widgets-right-maer');
-						break;
-
-						case 'mannerriege':
-						dynamic_sidebar('widgets-right-mr');
-						break;
-
-						case 'damenturnverein':
-						dynamic_sidebar('widgets-right-dtv');
-						break;
-
-						case 'elki-turnen':
-						dynamic_sidebar('widgets-right-elki');
-						break;
-
-						default:
-						echo '<p>Keine Seitenleiste eingerichtet</p>';
-						break;
-					}
-
-					?>
-
-				</div>
-
-			</div>
-			<!-- end widget area -->
 
 		</div>
-		<!-- end grid -->
+		<!-- end widget area -->
 
 	</div>
 	<!-- end page-main -->
 
 	<div id="page-bottom" class="bottomline">
 
-		<div class="grid-container">
+		<div class="flex-container">
 
 			<div class="grid">
 
